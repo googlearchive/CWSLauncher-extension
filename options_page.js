a1ae4697666e8634b88fe95fe7b8ce67b0ecb3bb
Joe Marini <joemarini@hotmail.com>
2012-12-17 09:19:23 -0800

    Initial commit
    Upload CWSLauncher code for review

---
+++ options_page.js
@@ -0,0 +1,77 @@
+/**
+ * @author Cesare Marini
+ */
+
+var appsList = [];
+
+// Initialize the favorites lists with the currently selected favorite apps
+function initFavorites() {
+    var oSelList = null, favID = "";
+    
+    for (var i=1; i <= 5; i++) {
+        favID = window.localStorage["fav"+i];
+        oSelList = document.getElementById('fav'+i);
+        if (favID != undefined) {
+            for (var j=0; j < oSelList.options.length; j++) {
+                if (oSelList.options[j].value == favID) {
+                    oSelList.options.selectedIndex = j;
+                    break;
+                }
+            }
+        }        
+    }
+}
+
+// When the user selects a favorite app, update the selection
+function updateFavorite(evt) {
+    var favID = this.id; // the id of the select element is the same as the id for the favorite in localStorage. Clever, huh?
+    var appID = this.options[this.selectedIndex].value;
+    if (appID != "---") {
+        window.localStorage[favID] = appID;
+    }
+    else
+        window.localStorage.removeItem(favID);
+}
+
+function compare(a, b) {
+    return (a > b) ? 1 : (a == b ? 0 : -1);
+}
+
+function compareByName(app1, app2) {
+    return compare(app1.name.toLowerCase(), app2.name.toLowerCase());
+}
+
+
+// Initialize the lists and set up the event listeners
+document.addEventListener('DOMContentLoaded', function() {
+    chrome.management.getAll(function(allAppData) {
+        // filter out items that are not apps
+        for (var i=0; i < allAppData.length; i++) {
+            if (allAppData[i].isApp) 
+                appsList.push(allAppData[i]);
+        }
+        appsList.sort(compareByName);
+        
+        // add the apps to the select lists
+        for (var i=1; i<=5; i++) {
+            var oSelList = document.getElementById('fav'+i);
+            var selOptions = "";
+            for (var j=0; j<appsList.length; j++) {
+                selOptions += "<option value=" + "'" + appsList[j].id + "'>" + appsList[j].name + "</option>";
+            }
+            oSelList.innerHTML += selOptions;
+        }
+        
+        // set the initial selections based on current favorites
+        initFavorites();
+        
+        // add the event listeners to the select lists
+        document.getElementById('fav1').addEventListener('change', updateFavorite);
+        document.getElementById('fav2').addEventListener('change', updateFavorite);
+        document.getElementById('fav3').addEventListener('change', updateFavorite);
+        document.getElementById('fav4').addEventListener('change', updateFavorite);
+        document.getElementById('fav5').addEventListener('change', updateFavorite);
+
+        document.getElementById('btnFinished').addEventListener('click', function() { window.close(); });
+    });
+});
---
+++ popup.js
@@ -0,0 +1,283 @@
+/**
+ * @author Cesare Marini
+ */
+
+// The list of all apps & extensions.
+var completeList = [];
+// A filtered list of apps we actually want to show.
+var appList = [];
+// The index of an app in |appList| that should be highlighted.
+var selectedIndex = 0;
+
+// Google Analytics code
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-4436568-7']);
+_gaq.push(['_trackPageview']);
+console.log("View tracked");
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+function $(id) {
+    return document.getElementById(id);
+}
+
+// Returns the largest size icon, or a default icon, for the given |app|.
+function getIconURL(app) {
+    if (!app || !app.icons || app.icons.length == 0) {
+        return chrome.extension.getURL('icon.png');
+    }
+    var largest = {
+        size : 0
+    };
+    for (var i = 0; i < app.icons.length; i++) {
+        var icon = app.icons[i];
+        if (icon.size > largest.size) {
+            largest = icon;
+        }
+    }
+    return largest.url;
+}
+
+function getAppByID(strID) {
+    for (var i=0; i < completeList.length; i++) {
+        if (completeList[i].id == strID)
+            return completeList[i];
+    }
+    return null;
+}
+
+function launchApp(id) {
+    chrome.management.launchApp(id);
+    window.close();
+    // Only needed on OSX because of crbug.com/63594
+}
+
+// Load the favorite applications. If there are none, then just pre-populate with the 
+// first five apps that are found.
+function loadFavorites() {
+    var defApp = 0;
+    for (var i=0; i < 5; i++) {
+        var curFav = "fav" + (i+1);
+        // Div IDs are the same as the localStorage keys
+        var curFavDiv = $(curFav);
+        var favAppID = window.localStorage[curFav];
+        var app=null;
+        var img = document.createElement('img');
+        img.height = 76;
+        img.width = 76;
+
+        // use one of the existing apps, or a default icon
+        if (!favAppID || favAppID != undefined) {
+            app = getAppByID(favAppID); //completeList[defApp++];
+            // if we have an appID for a favorite but the app comes back null
+            // (because the user uninstalled it, probably) then we need to remove
+            // that favorite ID from the list
+            if (!app) {
+                window.localStorage.removeItem(curFav);
+            }
+        }
+        
+        img.src = getIconURL(app); // returns default icon if app is null
+        curFavDiv.appendChild(img);
+        if (app) {
+            curFavDiv.title = app.name;
+            curFavDiv.className = "fav";
+            curFavDiv.setAttribute("data-appid",app.id);
+            curFavDiv.onclick = function() {
+                _gaq.push("CWSLauncher","favLaunch");
+                var strID = this.getAttribute("data-appid");
+                launchApp(strID);
+            };
+        }
+    }
+}
+
+// Adds DOM nodes for |app| into |appsDiv|.
+function addApp(appsDiv, app, selected) {
+    var div = document.createElement('div');
+    div.className = 'app' + ( selected ? ' app_selected' : '');
+    div.title = app.name;
+
+    div.onclick = function() {
+        _gaq.push("CWSLauncher","appLaunch");
+        launchApp(app.id);
+    };
+
+    var img = document.createElement('img');
+    img.src = getIconURL(app);
+    div.appendChild(img);
+
+    var title = document.createElement('span');
+    title.className = 'app_title';
+    title.innerText = app.name;
+    div.appendChild(title);
+
+    appsDiv.appendChild(div);
+}
+
+function reloadAppDisplay() {
+    var appsDiv = $('apps');
+
+    // Empty the current content.
+    appsDiv.innerHTML = '';
+
+    for (var i = 0; i < appList.length; i++) {
+        var item = appList[i];
+        addApp(appsDiv, item, i == selectedIndex);
+    }
+}
+
+// Puts only enabled apps from completeList into appList.
+function rebuildAppList(filter) {
+    selectedIndex = 0;
+    appList = [];
+    for (var i = 0; i < completeList.length; i++) {
+        var item = completeList[i];
+        // Skip extensions and disabled apps.
+        if (!item.isApp || !item.enabled) {
+            continue;
+        }
+        if (filter && item.name.toLowerCase().search(filter) < 0) {
+            continue;
+        }
+        appList.push(item);
+    }
+}
+
+// Shows the list of apps based on the search box contents.
+function onSearchInput() {
+    var filter = $('search').value;
+    rebuildAppList(filter);
+    reloadAppDisplay();
+}
+
+function compare(a, b) {
+    return (a > b) ? 1 : (a == b ? 0 : -1);
+}
+
+function compareByName(app1, app2) {
+    return compare(app1.name.toLowerCase(), app2.name.toLowerCase());
+}
+
+// Changes the selected app in the list.
+function changeSelection(newIndex) {
+    if (newIndex >= 0 && newIndex <= appList.length - 1) {
+        selectedIndex = newIndex;
+        reloadAppDisplay();
+
+        var selected = document.getElementsByClassName('app_selected')[0];
+        selected.scrollIntoView(false);
+    }
+}
+
+var keys = {
+    ENTER : 13,
+    ESCAPE : 27,
+    END : 35,
+    HOME : 36,
+    LEFT : 37,
+    UP : 38,
+    RIGHT : 39,
+    DOWN : 40
+};
+
+// Set up a key event handler that handles moving the selected app up/down,
+// hitting enter to launch the selected app, as well as auto-focusing the
+// search box as soon as you start typing.
+window.onkeydown = function(event) {
+    switch (event.keyCode) {
+        case keys.DOWN:
+        case keys.RIGHT:
+            changeSelection(selectedIndex + 1);
+            break;
+        case keys.UP:
+        case keys.LEFT:
+            changeSelection(selectedIndex - 1);
+            break;
+        case keys.HOME:
+            changeSelection(0);
+            break;
+        case keys.END:
+            changeSelection(appList.length - 1);
+            break;
+        case keys.ENTER:
+            if (document.activeElement == $('searchterms'))
+                searchStore();
+            else {
+                var app = appList[selectedIndex];
+                if (app) {
+                    _gaq.push("CWSLauncher","appLaunch");
+                    launchApp(app.id);
+                }
+            }
+            break;
+        default:
+            // Focus the search box and return true so you can just start typing even
+            // when the search box isn't focused.
+            // $('search').focus();
+            return true;
+    }
+    return false;
+};
+
+function searchStore() {
+    var searchURL = "https://chrome.google.com/webstore/search/";
+    var searchTerms = escape($('searchterms').value);
+    searchURL += searchTerms;
+    chrome.tabs.create({
+        'url' : searchURL,
+        'selected' : true
+    });
+}
+
+// Initalize the popup window.
+document.addEventListener('DOMContentLoaded', function() {
+    chrome.management.getAll(function(info) {
+        var appCount = 0;
+        var tempAppList = [];
+        for (var i = 0; i < info.length; i++) {
+            if (info[i].isApp) {
+                appCount++;
+                tempAppList.push(info[i]);
+            }
+        }
+        if (appCount == 0) {
+            $('search').style.display = 'none';
+            $('appstore_link').style.display = '';
+            return;
+        }
+        completeList = tempAppList.sort(compareByName);
+        onSearchInput();
+
+        loadFavorites();
+        $('search').focus();
+    });
+    
+    // Opens the webstore in a new tab.
+    document.querySelector('#visitstore').addEventListener('click', function() {
+        chrome.tabs.create({
+            'url' : 'https://chrome.google.com/webstore',
+            'selected' : true
+        });
+        window.close();
+    });
+    // Opens the Options view.
+    document.querySelector('#gotoOptions').addEventListener('click', function() {
+        chrome.tabs.create({
+            'url' : 'options_page.html',
+            'selected' : true
+        });
+        window.close();
+    });
+    // Searches the Web store, opens in a new tab.
+    document.querySelector('#searchstore').addEventListener('click', function() {
+        searchStore();
+        window.close();
+    });
+
+    $('search').addEventListener('input', onSearchInput);
+});
+
